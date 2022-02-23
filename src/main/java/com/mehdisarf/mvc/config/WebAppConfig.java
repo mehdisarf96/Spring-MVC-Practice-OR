a2db650272f544c1596f8b541703362663c270ba
M.Mehdi <m.sfj9696@gmail.com>
2022-02-24 03:26:20 +0330

    Register DispatcherServlet with Java

---
+++ src/main/java/com/mehdisarf/mvc/config/WebAppConfig.java
@@ -0,0 +1,11 @@
+package com.mehdisarf.mvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan("com.mehdisarf.mvc")
+@Configuration
+public class WebAppConfig {
+}
