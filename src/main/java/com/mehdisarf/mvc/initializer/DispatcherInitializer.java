2788470e197be9def47e1322071ef31f85d5ae2c
M.Mehdi <m.sfj9696@gmail.com>
2022-02-26 20:52:38 +0330

    Add an expedited and fast configuration for DispatcherServlet using AbstractAnnotationConfigDispatcherServletInitializer.

---
+++ src/main/java/com/mehdisarf/mvc/initializer/DispatcherInitializer.java
@@ -0,0 +1,22 @@
+package com.mehdisarf.mvc.initializer;
+
+import com.mehdisarf.mvc.config.WebAppConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
